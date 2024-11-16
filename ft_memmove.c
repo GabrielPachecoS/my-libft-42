0a3ceb61ebe954aa2c39436c6655af7f6980e203
Gabriel Souza <souzagabrielpacheco@gmail.com>
2024-11-16 11:18:17 -0300

    ft_memmove insert

---
+++ ft_memmove.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char		*d;
+	const unsigned char	*s;
+
+	if (!dest && !src)
+		return (NULL);
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	d += n;
+	s += n;
+	while (n--)
+		*(--d) = *(--s);
+	return (dest);
+}
