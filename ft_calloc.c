de88c182837701126ac8c8cc098a5b359e2aafa8
Gabriel Souza <souzagabrielpacheco@gmail.com>
2024-11-16 14:02:25 -0300

    ft_calloc insert

---
+++ ft_calloc.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	size_t	total_size;
+	void	*ptr;
+
+	total_size = nmemb * size;
+	if (nmemb && total_size / nmemb != size)
+		return (NULL);
+	ptr = malloc(total_size);
+	if (!ptr)
+		return (NULL);
+	ft_memset(ptr, 0, total_size);
+	return (ptr);
+}
