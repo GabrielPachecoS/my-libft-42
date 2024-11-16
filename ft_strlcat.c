aaa7f511f47e60a0ad16d57aad2692d7e36e3d74
Gabriel Souza <souzagabrielpacheco@gmail.com>
2024-11-16 11:41:21 -0300

    ft_strlcat insert

---
+++ ft_strlcat.c
@@ -0,0 +1,26 @@
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	total_len;
+	size_t	i;
+
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	i = 0;
+	if (size == 0)
+		return (src_len);
+	if (size > dst_len)
+		total_len = dst_len + src_len;
+	else
+		total_len = size + src_len;
+	while (src[i] && dst_len + i < size - 1)
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	return (total_len);
+}
